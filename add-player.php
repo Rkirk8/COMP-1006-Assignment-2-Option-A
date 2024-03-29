2516bd3ccf8a0961fc231a6fa9ef485aa88142e3
Rielly Kirk <Riellykirk13@gmail.com>
2024-03-29 17:41:34 -0400

    got the add players featcher working

--- add-player.php
+++ add-player.php
@@ -1,42 +1,44 @@
 <!--Shared Header-->
 <?php
-$title = 'Add New Player';
 include('Shared/header.php');
+$title = 'Add New Player';
+include('Shared/auth.php');
+
 ?>
-        <h1>Add New player</h1>
-        <!-- form for player name, age, position -->
-        <form method="post" action="player-saved.php">
-            <fieldset>
-                <label for="playerName">Name: </label>
-                <input name="playerName" id="playerName" placeholder="First Last" required/>
-            </fieldset>
-            <fieldset>
-                <label for="playerAge">Age: </label>
-                <input name="playerAge" id="playerAge" type="number" min="18" max="70"/>
-            </fieldset>
-            <!--Dropdown menu populated from the database-->
-            <fieldset>
-                <label for="playerPosition">Position: </label>
-                <select name="playerPosition" id="playerPosition" placeholder="Click me for dropdown" required>
-                    <?php
-                        include ('Shared/database.php');
-                        $sql = "SELECT positionName FROM positions";
-                        $result = $db->query($sql);
-                        foreach($result as $row){
-                            echo '<option value="' . $row['positionName'] . '">' . $row['positionName'] . '</option>';
-                        };
-                        $db = null;
-                    ?>
-                </select>
-            </fieldset>
-            <fieldset>
-                <label for="playerPhoto">Photo: </label>
-                <input type="file" name="playerPhoto" id="photo" accept="image/*"/>
-            </fieldset>
-            <!--submit button-->
-            <fieldset>
-                <button>Add New Player</button>
-            </fieldset>
-        </form>
-    </body>
+<h1>Add New player</h1>
+<!-- form for player name, age, position -->
+<form method="post" action="player-saved.php" enctype="multipart/form-data">
+    <fieldset>
+        <label for="playerName">Name: </label>
+        <input name="playerName" id="playerName" placeholder="First Last" required/>
+    </fieldset>
+    <fieldset>
+        <label for="playerAge">Age: </label>
+        <input name="playerAge" id="playerAge" type="number" min="18" max="70"/>
+    </fieldset>
+    <!--Dropdown menu populated from the database-->
+    <fieldset>
+        <label for="playerPosition">Position: </label>
+        <select name="playerPosition" id="playerPosition" placeholder="Click me for dropdown" required>
+            <?php
+            include ('Shared/db.php');
+            $sql = "SELECT positionName FROM positions";
+            $result = $db->query($sql);
+            foreach($result as $row){
+                echo '<option value="' . $row['positionName'] . '">' . $row['positionName'] . '</option>';
+            };
+            $db = null;
+            ?>
+        </select>
+    </fieldset>
+    <fieldset>
+        <label for="playerPhoto">Photo: </label>
+        <input type="file" name="playerPhoto" id="photo" accept="image/*"/>
+    </fieldset>
+    <!--submit button-->
+    <fieldset>
+        <button type="submit">Add New Player</button>
+    </fieldset>
+</form>
+</body>
 </html>
--- player-saved.php
+++ player-saved.php
@@ -7,81 +7,83 @@ include('Shared/header.php');
 // Connect to db
 include('Shared/db.php');
 
-// Extract player name from form data
-$playerName = $_POST['playerName'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Extract player name from form data
+    $playerName = $_POST['playerName'];
 
-// Check if player already exists in the database
-$sql = "SELECT * FROM players WHERE playerName = :playerName";
-$stat = $db->prepare($sql);
-$stat->execute(array(':playerName' => $playerName));
-$data = $stat->fetch();
+    // Check if player already exists in the database
+    $sql = "SELECT * FROM players WHERE playerName = :playerName";
+    $stat = $db->prepare($sql);
+    $stat->execute(array(':playerName' => $playerName));
+    $data = $stat->fetch();
 
-// If player is in the db, show error
-if ($data ) {
-    echo 'The player is already in the database';
-} else {
-    // Check if file was uploaded
-    if ($_FILES['playerPhoto']['size'] > 0) {
-        // User uploaded a photo
-        $photoName = $_FILES['playerPhoto']['name'];
-        $finalName = session_id() . '-' . $photoName; 
-
-        // Temp location in server cache
-        $tmp_name = $_FILES['playerPhoto']['tmp_name'];
-
-        // File type
-        $type = mime_content_type($tmp_name);
-
-        // Check if file type is valid
-        if ($type != 'image/jpeg' && $type != 'image/png') {
-            echo 'Photo must be a .jpg or .png';
-            exit();
-        } else {
-            move_uploaded_file($tmp_name, 'image/headshots/' . $finalName);
-        }
+    // If player is in the db, show error
+    if ($data ) {
+        echo 'The player is already in the database';
     } else {
-        $defaultPhoto = 'image/headshots/defaultHeadshot.png';
-        $finalName = session_id() . '-' . $defaultPhoto; 
-        copy($defaultPhoto, 'imege/headshots/' . $finalName);
-    }
+        // Check if file was uploaded
+        if ($_FILES['playerPhoto']['size'] > 0) {
+            // User uploaded a photo
+            $photoName = $_FILES['playerPhoto']['name'];
+            $finalName = session_id() . '-' . $photoName; 
 
-    // Capture form inputs into vars
-    $name = $_POST['playerName'];
-    $age = $_POST['playerAge'];
-    $position = $_POST['playerPosition'];
-    $ok = true;
+            // Temp location in server cache
+            $tmp_name = $_FILES['playerPhoto']['tmp_name'];
 
-    // Input validation before save
-    if (empty($name)) {
-        echo 'Name is required<br />';
-        $ok = false;
-    }
+            // File type
+            $type = mime_content_type($tmp_name);
 
-    if (empty($position)) {
-        echo 'Position is required<br />';
-        $ok = false;
-    }
+            // Check if file type is valid
+            if ($type != 'image/jpeg' && $type != 'image/png') {
+                echo 'Photo must be a .jpg or .png';
+                exit();
+            } else {
+                move_uploaded_file($tmp_name, 'image/headshots/' . $finalName);
+            }
+        } else {
+            $defaultPhoto = 'image/headshots/defaultHeadshot.png';
+            $finalName = session_id() . '-' . $defaultPhoto; 
+            copy($defaultPhoto, 'imege/headshots/' . $finalName);
+        }
 
-    if ($ok) {
-        try {
-            // Set up SQL INSERT command
-            $sql = "INSERT INTO players (playerName, playerAge, position, photo) 
-                    VALUES (:name, :age, :position, :photo)";
-            // Prepare the statement
-            $cmd = $db->prepare($sql);
-            // Map each input to a column in the players table
-            $cmd->bindParam(':name', $name, PDO::PARAM_STR, 100);
-            $cmd->bindParam(':age', $age, PDO::PARAM_INT);
-            $cmd->bindParam(':position', $position, PDO::PARAM_STR, 50);
-            $cmd->bindParam(':photo', $finalName, PDO::PARAM_STR, 100);
+        // Capture form inputs into vars
+        $name = $_POST['playerName'];
+        $age = $_POST['playerAge'];
+        $position = $_POST['playerPosition'];
+        $ok = true;
 
-            // Execute the INSERT (which saves to the db)
-            $cmd->execute();
+        // Input validation before save
+        if (empty($name)) {
+            echo 'Name is required<br />';
+            $ok = false;
+        }
 
-            // Show message to user
-            echo 'Player Saved';
-        } catch (Exception $err) {
-            echo 'Error saving player: ' . $err->getMessage();
+        if (empty($position)) {
+            echo 'Position is required<br />';
+            $ok = false;
+        }
+
+        if ($ok) {
+            try {
+                // Set up SQL INSERT command
+                $sql = "INSERT INTO players (playerName, playerAge, position, photo) 
+                        VALUES (:name, :age, :position, :photo)";
+                // Prepare the statement
+                $cmd = $db->prepare($sql);
+                // Map each input to a column in the players table
+                $cmd->bindParam(':name', $name, PDO::PARAM_STR, 100);
+                $cmd->bindParam(':age', $age, PDO::PARAM_INT);
+                $cmd->bindParam(':position', $position, PDO::PARAM_STR, 50);
+                $cmd->bindParam(':photo', $finalName, PDO::PARAM_STR, 100);
+
+                // Execute the INSERT (which saves to the db)
+                $cmd->execute();
+
+                // Show message to user
+                echo 'Player Saved';
+            } catch (Exception $err) {
+                echo 'Error saving player: ' . $err->getMessage();
+            }
         }
     }
 }
